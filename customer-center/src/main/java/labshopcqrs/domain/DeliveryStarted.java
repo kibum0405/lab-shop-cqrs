5136c05c6bbfb7d3f45873d5df6acacfda05d144
kibum0405 <59447401+kibum0405@users.noreply.github.com>
2022-10-13 16:14:35 +0900

    commit

---
+++ customer-center/src/main/java/labshopcqrs/domain/DeliveryStarted.java
@@ -0,0 +1,14 @@
+package labshopcqrs.domain;
+
+import labshopcqrs.infra.AbstractEvent;
+import lombok.Data;
+import java.util.*;
+
+@Data
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+}
---
+++ delivery/src/main/java/labshopcqrs/domain/DeliveryStarted.java
@@ -0,0 +1,23 @@
+package labshopcqrs.domain;
+
+import labshopcqrs.domain.*;
+import labshopcqrs.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+
+    public DeliveryStarted(Delivery aggregate){
+        super(aggregate);
+    }
+    public DeliveryStarted(){
+        super();
+    }
+}
